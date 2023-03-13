711cf2a9e07a1005c308f31906883bfd594df5e8
Taz17 <mutaz.ashhab@gmail.com>
2023-03-13 17:58:06 +1100

    add `draw` function, call it on render, and change styling

--- src/Components/Canvas.tsx
+++ src/Components/Canvas.tsx
@@ -1,12 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const canvasScale = window.devicePixelRatio;
+  // const canvasScale = window.devicePixelRatio;
 
-  const canvasHeight = 800 * canvasScale; // temporarily hardcoded values
-  const canvasWidth = 800 * canvasScale; // temporarily hardcoded values
+  // const canvasHeight = 800 * canvasScale; // temporarily hardcoded values
+  // const canvasWidth = 800 * canvasScale; // temporarily hardcoded values
 
   const handleContextMenu: (event: React.MouseEvent) => void = (
     event: React.MouseEvent,
@@ -57,12 +57,47 @@ const Canvas = () => {
     return;
   };
 
+  const draw: () => void = () => {
+    const canvas = canvasRef.current;
+    const GRID_SIZE = 50;
+
+    if (canvas === null) return;
+
+    const ctx = canvas.getContext('2d');
+
+    if (ctx === null) return;
+
+    const left = 0.5 - Math.ceil(canvas.width / GRID_SIZE) * GRID_SIZE;
+    const top = 0.5 - Math.ceil(canvas.height / GRID_SIZE) * GRID_SIZE;
+    const right = 2 * canvas.width;
+    const bottom = 2 * canvas.height;
+
+    ctx.clearRect(left, top, right - left, bottom - top);
+    ctx.beginPath();
+
+    for (let x = left; x < right; x += GRID_SIZE) {
+      ctx.moveTo(x, top);
+      ctx.lineTo(x, bottom);
+    }
+
+    for (let y = top; y < bottom; y += GRID_SIZE) {
+      ctx.moveTo(left, y);
+      ctx.lineTo(right, y);
+    }
+
+    ctx.strokeStyle = '#888';
+    ctx.stroke();
+  };
+
+  useEffect(() => {
+    draw();
+  }, []);
+
   return (
     <canvas
       ref={canvasRef}
       style={{
-        width: canvasHeight,
-        height: canvasWidth,
+        background: 'silver',
       }}
       onContextMenu={handleContextMenu}
       onPointerDown={handlePointerDown}
